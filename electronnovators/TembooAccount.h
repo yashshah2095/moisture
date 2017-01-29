a088c4e56eed1da18405b3c17350edc9d7bd6c89
yashshah2095 <yashshah2095@gmail.com>
2017-01-29 14:19:12 +0530

    Code generated by Temboo

---
+++ electronnovators/TembooAccount.h
@@ -0,0 +1,21 @@
+/*
+IMPORTANT NOTE about TembooAccount.h
+
+TembooAccount.h contains your Temboo account information and must be included
+alongside your sketch. To do so, make a new tab in Energia, call it TembooAccount.h,
+and copy this content into it. 
+*/
+
+#define TEMBOO_ACCOUNT "accountName"  // Your Temboo account name 
+#define TEMBOO_APP_KEY_NAME "myFirstApp"  // Your Temboo app name
+#define TEMBOO_APP_KEY "abc123xxxxxxxxxxxxxx"  // Your Temboo app key
+
+#define WIFI_SSID ""
+#define WPA_PASSWORD ""
+
+/* 
+The same TembooAccount.h file settings can be used for all Temboo sketches.
+
+Keeping your account information in a separate file means you can share the 
+main .ino file without worrying that you forgot to delete your credentials.
+*/
